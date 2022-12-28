3b2294cbc0f35345c8b1d1b76036112c8ee0bb98
ashuuu <ashwinipurma576@gmail.com>
2022-12-28 19:32:05 +0530

    added table and schemas

---
+++ SQL/ace_operation/pre_deployment/1.schema_and_groups_creation.sql
@@ -0,0 +1,20 @@
+create schema ace_operation;
+
+create group ace_operation_cons_looker_rl;
+create group ace_operation_cons_updater_rl;
+create group ace_operation_cons_owner_rl;
+
+grant usage on schema ace_operation to group ace_operation_cons_looker_rl;
+grant usage on schema ace_operation to group ace_operation_cons_updater_rl;
+grant usage, create on schema ace_operation to group ace_operation_cons_owner_rl;
+
+alter default privileges in schema ace_operation grant select on tables to group ace_operation_cons_looker_rl;
+
+alter default privileges in schema ace_operation grant select,update,insert,delete on tables to group ace_operation_cons_updater_rl;
+alter default privileges in schema ace_operation grant execute on functions to group ace_operation_cons_updater_rl;
+alter default privileges in schema ace_operation grant execute on procedures to group ace_operation_cons_updater_rl;
+
+alter default privileges in schema ace_operation grant all on tables to group ace_operation_cons_owner_rl;
+alter default privileges in schema ace_operation grant all on functions to group ace_operation_cons_owner_rl;
+alter default privileges in schema ace_operation grant all on procedures to group ace_operation_cons_owner_rl;
+
